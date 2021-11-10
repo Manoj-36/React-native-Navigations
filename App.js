492f93e0c3f4be7b8b03cee5cd457908171146a0
Manoj-36 <n1k0l4t35l4369@gmail.com>
2021-11-10 14:42:14 +0530

    bottom navigation is added

--- App.js
+++ App.js
@@ -1,12 +1,104 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+// import React from 'react';
+import * as React from 'react';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
+
+import {FontAwesome5} from '@expo/vector-icons'
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Tab.Navigator tabBarOptions={{
+        showLabel: false,
+        
+      }}>
+          {
+
+          }
+          <Tab.Screen name={"Home"} component={HomeScreen} options={{
+            tabBarIcon: ({focused}) => (
+              <View>
+                <FontAwesome5 name="home" size={25}
+                color={focused ? 'black' : 'gray'}
+                ></FontAwesome5>
+                {/* <AntDesign name="home" size={25} color={focused ? 'red' : 'gray'}/> */}
+              </View>
+            )
+          }} ></Tab.Screen>
+
+          <Tab.Screen name={"Search"} component={ChatScreen} options={{
+            tabBarIcon: ({focused}) => (
+              <View>
+                <FontAwesome5 name="search" size={25}
+                color={focused ? 'black' : 'gray'}
+                ></FontAwesome5>
+                {/* <AntDesign name="search1" size={25} color="black" /> */}
+              </View>
+            )
+          }}></Tab.Screen>
+
+          <Tab.Screen name={"Notification"} component={NotificationScreen} options={{
+            tabBarIcon: ({focused}) => (
+              <View>
+                <FontAwesome5 name="bell" size={25}
+                color={focused ? 'black' : 'gray'}
+                ></FontAwesome5>
+                {/* <AntDesign name="home" size={24} color="black" /> */}
+              </View>
+            )
+          }}></Tab.Screen>
+
+          <Tab.Screen name={"settings"} component={SettingsScreen} options={{
+            tabBarIcon: ({focused}) => (
+              <View>
+                {/* <AntDesign name="home" size={24} color="black" /> */}
+                <Ionicons name="person" size={25}
+                color={focused ? 'black' : 'gray'}
+                 />
+              </View>
+            )
+          }}></Tab.Screen>
+
+      </Tab.Navigator>
+
+    </NavigationContainer>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+function NotificationScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notification</Text>
+    </View>
+  );
+}
+
+function ChatScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>chat</Text>
     </View>
   );
 }
